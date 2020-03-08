fa2a4e2fdbdad8ee0b8d83cc584abff8d170ccfd
Satu Hyle <shyle90@gmail.com>
2020-03-08 14:49:19 +0200

    Lab 2 completed

--- app/src/main/java/com/example/mobcomplab1/MainActivity.kt
+++ app/src/main/java/com/example/mobcomplab1/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.mobcomplab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobcomplab1.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_time.setOnClickListener{
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        button_map.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
+
+        button_special.setOnClickListener{
+            startActivity(Intent(applicationContext, SpecialActivity::class.java))
+        }
     }
 }
