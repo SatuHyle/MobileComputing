d521f0737f66fe6b7947f4ee63d5c59c359e4afa
Satu Hyle <shyle90@gmail.com>
2020-03-15 17:03:18 +0200

    Lab5 completed

---
+++ app/src/main/java/com/example/mobcomplab/GeofenceReciever.kt
@@ -0,0 +1,26 @@
+package com.example.mobcomplab
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+
+class GeofenceReciever : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        val geofencingTransition = geofencingEvent.geofenceTransition
+
+        if (geofencingTransition==Geofence.GEOFENCE_TRANSITION_ENTER
+            ||geofencingTransition==Geofence.GEOFENCE_TRANSITION_DWELL
+        ){
+            var uid = intent!!.getIntExtra("uid",0)
+            val text = intent.getStringExtra("message")
+
+            MainActivity.showNotification(context!!, text)
+
+        }
+    }
+}
--- app/src/main/java/com/example/mobcomplab/ReminderReceiver.kt
+++ app/src/main/java/com/example/mobcomplab/ReminderReceiver.kt
@@ -3,13 +3,24 @@ package com.example.mobcomplab
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.room.Room
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 
 class ReminderReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
 
         val text = intent.getStringExtra("message")
-        context.toast(text!!)
+        val uid = intent.getIntExtra("uid",0)
+        //context.toast(text!!)
+
+        MainActivity.showNotification(context,text!!)
+
+        doAsync {
+            val db = Room.databaseBuilder(context, AppDatabase::class.java,"reminders").build()
+            db.reminderDao().delete(uid)
+            db.close()
+        }
     }
 
 }
